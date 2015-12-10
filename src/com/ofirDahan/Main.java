6fbdcbf30ba532d2e63aa41523e366e0b0943a14
Dahan <odahan@6c4008a72d12.ant.amazon.com>
2015-12-10 11:20:35 -0800

    Splitting text

---
+++ src/com/ofirDahan/Main.java
@@ -0,0 +1,45 @@
+package com.ofirDahan;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+import static com.ofirDahan.wordCleanse.articleRemover;
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+
+        Map<String, Integer> finalCount = new TreeMap<>();
+
+        Path Stan_path = Paths.get("/Users/odahan/Desktop", "Stan.txt");
+
+        Charset charset = Charset.forName("UTF-8");
+
+        try {
+            List<String> lines = Files.readAllLines(Stan_path, charset);
+
+            for (String line : lines) {
+                
+                finalCount = wordCleanse.wordMap(line, articleRemover());
+            }
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        System.out.println(finalCount);
+    }
+
+}
+
+
+
+
+
+
---
+++ src/com/ofirDahan/wordCleanse.java
@@ -0,0 +1,61 @@
+package com.ofirDahan;
+
+import java.util.*;
+
+public class wordCleanse {
+
+    public static Map<String, Integer> wordCounter = new TreeMap<>();
+
+    public static Set<String> articleRemover(){
+
+        Set<String> articleSet = new TreeSet<>();
+
+        articleSet.add("a");
+        articleSet.add("and");
+        articleSet.add("ain't");
+        articleSet.add("are");
+        articleSet.add("the");
+        articleSet.add("one");
+        articleSet.add("an");
+        articleSet.add("as");
+        articleSet.add("about");
+        articleSet.add("em");
+        articleSet.add("at");
+        articleSet.add("be");
+        articleSet.add("all");
+        articleSet.add("but");
+        articleSet.add("by");
+        articleSet.add("do");
+        articleSet.add("each");
+        articleSet.add("else");
+        articleSet.add("for");
+        articleSet.add("'cause");
+        articleSet.add("bout");
+
+        return articleSet;
+    }
+
+
+    public static  Map<String, Integer> wordMap(String line, Set<String> articleRemover) {
+
+        String[] splitText = line.toLowerCase().split("([^a-zA-Z']+)'*\\1*");
+
+        List<String> word = Arrays.asList(splitText);
+
+
+        for(String s: word){
+            if(s.length() >= 2 && !articleRemover.contains(s)) {
+                if (wordCounter.containsKey(s)) {
+                    int currentValue = wordCounter.get(s);
+                    currentValue++;
+                    wordCounter.put(s, currentValue);
+                } else {
+                    wordCounter.put(s, 1);
+                }
+            }
+        }
+
+        return wordCounter;
+    }
+
+}
